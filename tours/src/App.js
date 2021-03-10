aa7b8871c986e6c3590c2567a9648529211a33ad
unknown <rahulmaurya032@gmail.com>
2021-03-10 14:06:36 +0530

    complete tour

--- tours/src/App.js
+++ tours/src/App.js
@@ -8,6 +8,11 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id)=>{
+    const newTour = tours.filter((tour)=> id !== tour.id)
+    setTours(newTour);
+  }
+
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -31,9 +36,19 @@ function App() {
       </main>
     );
   }
+  if(tours.length === 0){
+    return (
+      <main>
+        <div className="title">
+          <h2>No Tour left</h2>
+          <button className = "btn" onClick={fetchTours}>Refreash Tour</button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      <Tours tours = {tours} />
+      <Tours tours = {tours} removeTour = {removeTour}/>
     </main>
   );
 }
--- tours/src/Tour.js
+++ tours/src/Tour.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 
-const Tour = (props) => {
+const Tour = ({id,image,info , price ,name , removeTour}) => {
+  const [readMore , setReadMore] = useState(false)
   return (
     <section className = "single-tour">
-      <img src={props.image} alt={props.name}/>
+      <img src={image} alt={name}/>
       <footer>
         <div className="tour-info">
-          <h4>{props.name}</h4>
-          <h4 className="tour-price">${props.price}</h4>
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4> 
         </div>
+        <p>
+          {readMore ? info : `${info.substring(0,200)}...`}
+          <button className="read-btn" onClick={()=>setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
+          </button>
+          </p>
+        <button className="delete-btn"  onClick = {()=>removeTour(id)} > Not Interested</button>
       </footer>
 
 
